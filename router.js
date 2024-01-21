987b430d3dcd42d90cb98e3b06d453d8e16c8839
rithasahmed12 <145911740+rithasahmed12@users.noreply.github.com>
2024-01-21 09:49:42 +0530

    First Commit

---
+++ router.js
@@ -0,0 +1,52 @@
+const express=require("express");
+const  router=express.Router();
+
+const credential={
+    email:"admin@gmail.com",
+    password:"Admin123@"
+}
+
+// Home route
+router.get('/', (req, res) => {
+    
+    let message = req.session.message
+    req.session.message = ''
+    if(req.session.user){
+        res.redirect("/dashboard")
+    }else{
+        res.render('base', { title: "Login System",message } );
+    }
+});
+
+
+// login user
+router.post('/login',(req,res)=>{
+    if(req.body.email == credential.email && req.body.password == credential.password){
+        req.session.user = req.body.email;
+        console.log(req.session.user,"this is session");
+        res.redirect('/dashboard')
+        console.log(req.session);
+        // res.end("login Successful....!")
+    }else{
+        req.session.message = 'invalid user'
+        res.redirect("/")
+    }
+
+})
+// rote for dashboard
+router.get('/dashboard',(req,res)=>{
+    if(req.session.user){
+            // Render the dashboard
+        res.render('dashboard',{user : req.session.user})
+    }else{
+        res.redirect("/")
+    }
+})
+
+// route for logout
+router.get('/logout',(req,res)=>{
+    req.session.destroy()
+    res.redirect("/")
+})
+
+module.exports=router;
